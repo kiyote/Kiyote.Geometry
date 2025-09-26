003c68714759397671cd08de53bbce595f6d398d
Todd Lang <todd.lang@d2l.com>
2025-09-26 13:36:14 -0400

    Adding DI helper for noises

---
+++ src/Kiyote/Geometry/Kiyote.Geometry/Noises/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Kiyote.Geometry.Noises;
+
+public static class ExtensionMethods {
+
+	public static IServiceCollection AddNoise(
+		this IServiceCollection services
+	) {
+		services.TryAddSingleton<INoisyEdgeFactory, MidpointDisplacementNoisyEdgeFactory>();
+
+		return services;
+	}
+}
